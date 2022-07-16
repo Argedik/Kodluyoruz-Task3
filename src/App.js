3b5871e13f0439040dae5f5c4e3154aaec2e5e59
Enes Yasin Gedik <gedikas@hotmail.com>
2022-07-16 09:55:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,36 +3,18 @@ import MainPage from "./pages/MainPage";
 import "./App.css";
 import GamePage from "./pages/GamePage";
 import ResultPage from "./pages/ResultPage";
-// const HomePage = () => <div>HomePage</div>;
-
 function App() {
+  //Rooter yapısı oluşturuluyor
   return (
     <>
       <Routes>
-        {/* <Route path="/" element={<MainPage />} /> */}
-        {/* <Route path="/" element={<MainPage />} /> */}
         <Route path="/" element={<MainPage />} />
         <Route path="/Kodluyoruz-Task3" element={<MainPage />} />
         <Route path="/GamePage" element={<GamePage />} />
         <Route path="/ResultPage" element={<ResultPage />} />
-        {/* <Route path="/home" element={<MainPage />} /> */}
       </Routes>
     </>
   );
 }
 
 export default App;
-/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */
